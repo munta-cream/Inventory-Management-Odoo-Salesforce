8874d0d1b08ec7a131e5667c8a614bd591c313cd
Muntakim <97120249+munta-cream@users.noreply.github.com>
2025-09-17 02:56:14 +0600

    Add files via upload

---
+++ obj/Debug/net8.0/Inventory Management Requirements.AssemblyInfo.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Reflection;
+
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Inventory Management Requirements")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Inventory Management Requirements")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Inventory Management Requirements")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
+
